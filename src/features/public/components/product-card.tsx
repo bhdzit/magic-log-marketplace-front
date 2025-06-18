6a34d7c0656d1af7d08bd6539627b7d75836d13c
Bryan Hernandez <bhdzit97@gmail.com>
2025-06-17 21:14:27 -0600

    Product card base layout

---
+++ src/features/public/components/product-card.tsx
@@ -0,0 +1,28 @@
+import type { IProduct } from "../../../utils/interfaces/product.interfaces";
+
+export function ProductCard({ name, img, price }: IProduct) {
+  return (
+    <div className="flex h-full flex-1 flex-col gap-4 rounded-xl bg-white shadow-[0_0_4px_rgba(0,0,0,0.1)] min-w-60">
+      <img
+        className="w-full bg-center bg-no-repeat aspect-square bg-cover rounded-xl flex flex-col"
+        src={img}
+      />
+      <div className="flex flex-col flex-1 justify-between p-4 pt-0 gap-4">
+        <div>
+          <p className="text-[#111418] text-base font-medium leading-normal">
+            {name}
+          </p>
+          <p className="text-[#637488] text-sm font-normal leading-normal">
+            {new Intl.NumberFormat("en-IN", {
+              style: "currency",
+              currency: "USD",
+            }).format(price)}
+          </p>
+        </div>
+        <button className="flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-full h-10 px-4 bg-[#f0f2f4] text-[#111418] text-sm font-bold leading-normal tracking-[0.015em]">
+          <span className="truncate">Add to Cart</span>
+        </button>
+      </div>
+    </div>
+  );
+}
---
+++ src/utils/interfaces/product.interfaces.ts
@@ -0,0 +1,8 @@
+export interface IProduct {
+  _id?: string;
+  name: string;
+  sku: string;
+  price: number;
+  stock: number;
+  img: string;
+}
